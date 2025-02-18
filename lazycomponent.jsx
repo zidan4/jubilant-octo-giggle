6aa46969fc8abc98a97204e2d0eb4c026b6c2f4b
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-02-18 17:18:59 +0300

    Update lazycomponent.jsx

--- lazycomponent.jsx
+++ lazycomponent.jsx
@@ -1,1 +1,10 @@
 
+import React from 'react';
+
+export default function LazyComponent() {
+  return (
+    <div style={{ marginTop: '20px', color: 'blue' }}>
+      This is a lazily loaded component!
+    </div>
+  );
+}
